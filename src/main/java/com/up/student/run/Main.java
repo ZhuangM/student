baeedea5f979b5ae499ee54a8fce41a30a224cae
devintang <yonghua_t@qq.com>
2018-08-22 15:48:11 +0800

    嵌入sqlite数据库，使其成为不再有外围依赖的独立应用

--- src/main/java/com/up/student/run/Main.java
+++ src/main/java/com/up/student/run/Main.java
@@ -6,6 +6,7 @@
  */
 package com.up.student.run;
 
+import com.up.student.util.DBUtil;
 import com.up.student.view.LoginView;
 
 /**
@@ -13,7 +14,38 @@ import com.up.student.view.LoginView;
  * 
  */
 public class Main {
+	public static void initDB() {
+		DBUtil dbUtil = DBUtil.getDBUtil();
+
+		//检查数据库是否初始化
+		if (dbUtil.exeute("select 1 from  admin")) {
+			return;
+		}
+
+		//初始化数据库
+		//admin表
+		dbUtil.exeute("create table if not exists admin(id int primary key," +
+				"name varchar(32)," +
+				"username varchar(32)," +
+				"password varchar(32))");
+		dbUtil.exeute("insert into admin(id, name, username, password) values(1, 'admin', 'test', 'test')");
+
+		//student
+		dbUtil.exeute("create table if not exists student(" +
+				"id int primary key," +
+				"sno varchar(16)," +
+				"name varchar(32)," +
+				"sex varchar(8)," +
+				"department varchar(32)," +
+				"hometown varchar(64)," +
+				"mark varchar(32)," +
+				"email varchar(32)," +
+				"tel varchar(16))");
+	}
+
 	public static void main(String[] args) {
+		initDB();
 		new LoginView();
+		DBUtil.getDBUtil().close();
 	}
 }
